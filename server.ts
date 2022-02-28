b8313bc7760527390c486b551b5fceba343ba925
daniellwdb <44723767+daniellwdb@users.noreply.github.com>
2022-02-28 18:06:31 +0100

    feat(server): cache and log requests conditionally

--- server.ts
+++ server.ts
@@ -5,26 +5,40 @@ import { createRequestHandler } from "@mcansh/remix-fastify";
 
 import * as serverBuild from "@remix-run/dev/server-build";
 
+const MODE = process.env.NODE_ENV;
+
 async function startServer() {
-  const fastify = Fastify({ logger: true });
+  const fastify = Fastify({
+    logger: true,
+    disableRequestLogging: process.env.ENABLE_REQUEST_LOGGING !== "true",
+  });
 
   await fastify.register(fastifyExpress);
 
   // Remix fingerprints its assets so we can cache forever.
   fastify.use(
     "/build",
-    sirv("public/build", { immutable: true, maxAge: 31536000 })
+    sirv("public/build", {
+      dev: MODE !== "production",
+      immutable: true,
+      maxAge: 31536000,
+    })
   );
 
   // Everything else (like favicon.ico) is cached for an hour. You may want to be
   // more aggressive with this caching.
-  fastify.use(sirv("public/build", { maxAge: 3600 }));
+  fastify.use(
+    sirv("public", {
+      dev: MODE !== "production",
+      maxAge: 3600,
+    })
+  );
 
   fastify.all(
     "*",
     createRequestHandler({
       build: serverBuild,
-      mode: process.env.NODE_ENV,
+      mode: MODE,
     })
   );
 
